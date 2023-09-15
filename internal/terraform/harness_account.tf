33e4e4963f40c8161ba368ee6201f5e1aa102950
Riley Snyder <riley.snyder@harness.io>
2023-09-15 10:18:28 -0500

    chore: import harness community resources in tf

--- internal/terraform/harness_account.tf
+++ internal/terraform/harness_account.tf
@@ -1,13 +1,3 @@
-resource "harness_platform_variables" "sales_aws_delegate_role" {
-  identifier = "sales_aws_delegate_role"
-  name       = "sales_aws_delegate_role"
-  type       = "String"
-  spec {
-    value_type  = "FIXED"
-    fixed_value = aws_iam_role.delegate-assumed.arn
-  }
-}
-
 resource "harness_platform_connector_kubernetes" "sa-cluster" {
   identifier = "sacluster"
   name       = "sa-cluster"
---
+++ internal/terraform/harness_community.tf
@@ -0,0 +1,68 @@
+resource "harness_platform_organization" "Harness_Community" {
+  identifier  = "Harness_Community"
+  name        = "Harness Community"
+  description = "For examples that live in the Harness-Community Github repo."
+  tags        = ["managed:terraform"]
+}
+
+resource "harness_platform_project" "utilities" {
+  identifier = "utilities"
+  name       = "utilities"
+  org_id     = harness_platform_organization.Harness_Community.id
+  tags       = ["managed:terraform"]
+}
+
+resource "harness_platform_project" "setup" {
+  identifier = "setup"
+  name       = "setup"
+  org_id     = harness_platform_organization.Harness_Community.id
+  tags       = ["managed:terraform"]
+}
+
+resource "harness_platform_project" "examples" {
+  identifier = "examples"
+  name       = "examples"
+  org_id     = harness_platform_organization.Harness_Community.id
+  tags       = ["managed:terraform"]
+}
+
+
+resource "harness_platform_connector_github" "harness_community_github" {
+  identifier = "harness_community_github"
+  name       = "harness community"
+  tags       = ["managed:terraform"]
+  org_id     = harness_platform_organization.Harness_Community.id
+
+  url             = "https://github.com/harness-community"
+  connection_type = "Account"
+  validation_repo = "solutions-architecture"
+
+  credentials {
+    http {
+      username  = "solutions-architects"
+      token_ref = "org.harnesscommunity_sa_app_token"
+    }
+  }
+  api_authentication {
+    github_app {
+      installation_id = "40515231"
+      application_id  = "314848"
+      private_key_ref = "org.harness_solution_architecture_github_app_cert"
+    }
+  }
+}
+
+resource "harness_platform_connector_docker" "harness_community_dockerhub" {
+  identifier = "harness_community_dockerhub"
+  name       = "harness community"
+  tags       = ["managed:terraform"]
+  org_id     = harness_platform_organization.Harness_Community.id
+
+  type = "DockerHub"
+  url  = "https://index.docker.io/v2/"
+
+  credentials {
+    username     = "harnesscommunity"
+    password_ref = "org.harnesscommunity_dockerhub"
+  }
+}
