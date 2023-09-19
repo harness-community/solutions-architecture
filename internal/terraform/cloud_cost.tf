8c8719b36f5ef6ea75ac5fbbdfdb6d29edfb377e
Riley Snyder <riley.snyder@harness.io>
2023-09-19 11:53:16 -0500

    create ccm resources

--- internal/terraform/cloud_cost.tf
+++ internal/terraform/cloud_cost.tf
@@ -1,10 +1,76 @@
+resource "aws_s3_bucket" "solutions-architecture" {
+  bucket = "solutions-architecture"
+
+  tags = {
+    Owner = "sa@harness.io"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "solutions-architecture" {
+  bucket = aws_s3_bucket.solutions-architecture.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "solutions-architecture" {
+  bucket = aws_s3_bucket.solutions-architecture.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_cur_report_definition" "solutions-architecture" {
+  report_name                = "solutions-architecture"
+  time_unit                  = "HOURLY"
+  format                     = "textORcsv"
+  compression                = "GZIP"
+  additional_schema_elements = ["RESOURCES"]
+  s3_bucket                  = aws_s3_bucket.solutions-architecture.bucket
+  s3_region                  = aws_s3_bucket.solutions-architecture.region
+  s3_prefix                  = "ccm"
+  additional_artifacts       = []
+  report_versioning          = "OVERWRITE_REPORT"
+}
+
+module "ccm" {
+  source  = "harness-community/harness-ccm/aws"
+  version = "0.1.1"
+
+  s3_bucket_arn = aws_s3_bucket.solutions-architecture.arn
+  external_id   = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
+  additional_external_ids = [
+    "harness:891928451355:V2iSB2gRR_SxBs0Ov5vqCQ"
+  ]
+  enable_billing          = true
+  enable_events           = true
+  enable_optimization     = true
+  enable_governance       = true
+  enable_commitment_read  = true
+  enable_commitment_write = true
+  governance_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess"
+  ]
+  prefix = "sa-"
+  secrets = [
+    "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-key.pem-HYlaV4",
+    "arn:aws:secretsmanager:us-west-2:759984737373:secret:sa/ca-cert.pem-kq8HQl"
+  ]
+}
+
 resource "harness_platform_connector_awscc" "sales_aws_cost" {
   identifier = "sales_aws_cost"
   name       = "sales_aws_cost"
 
   account_id  = "759984737373"
-  report_name = "riley-harness-ccm"
-  s3_bucket   = "riley-harness-ccm"
+  report_name = aws_cur_report_definition.solutions-architecture.report_name
+  s3_bucket   = aws_s3_bucket.solutions-architecture.bucket
 
   features_enabled = [
     "OPTIMIZATION",
@@ -12,10 +78,9 @@ resource "harness_platform_connector_awscc" "sales_aws_cost" {
     "BILLING",
   ]
 
-  # using CCM setup from riley's personal Harness account
   cross_account_access {
-    role_arn    = "arn:aws:iam::759984737373:role/riley-HarnessCERole"
-    external_id = "harness:891928451355:wlgELJ0TTre5aZhzpt8gVA"
+    role_arn    = module.ccm.cross_account_role
+    external_id = module.ccm.external_id
   }
 }
 
