20c03227afa0349172b5652b21a53aadf31d6be0
Riley Snyder <riley.snyder@harness.io>
2023-09-19 13:59:55 -0500

    add role assignments

--- internal/terraform/azure.tf
+++ internal/terraform/azure.tf
@@ -1,3 +1,9 @@
+locals {
+  harness_azure_application_id = "0211763d-24fb-4d63-865d-92f86f77e908"
+}
+
+data "azurerm_subscription" "current" {}
+
 resource "azurerm_resource_group" "solutions-architecture" {
   name     = "solutions-architecture"
   location = "Central US"
@@ -15,4 +21,22 @@ resource "azurerm_storage_container" "ccm" {
   name                  = "ccm"
   storage_account_name  = azurerm_storage_account.solutions-architecture.name
   container_access_type = "private"
+}
+
+resource "azurerm_role_assignment" "solutions-architecture-ccm" {
+  scope                = azurerm_storage_account.solutions-architecture.id
+  role_definition_name = "Storage Blob Data Reader"
+  principal_id         = local.harness_azure_application_id
+}
+
+resource "azurerm_role_assignment" "solutions-architecture-reader" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Reader"
+  principal_id         = local.harness_azure_application_id
+}
+
+resource "azurerm_role_assignment" "solutions-architecture-contributor" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Contributor"
+  principal_id         = local.harness_azure_application_id
 }
