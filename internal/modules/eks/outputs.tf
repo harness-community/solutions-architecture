1e2bdd61a2cb8f251ec6d887cb31904e5735f356
Riley Snyder <riley.snyder@harness.io>
2024-07-01 13:42:15 -0500

    add more outputs for connectors

--- internal/modules/eks/outputs.tf
+++ internal/modules/eks/outputs.tf
@@ -14,3 +14,18 @@ output "delegate_name" {
   value = local.delegate_name
 }
 
+output "k8s_connector_id" {
+  value = harness_platform_connector_kubernetes.sales_eks.id
+}
+
+output "ccm_k8s_connector_id" {
+  value = harness_platform_connector_kubernetes_cloud_cost.sales_eks_ccm.id
+}
+
+output "aws_connector_id" {
+  value = harness_platform_connector_aws.sales_eks_aws.id
+}
+
+output "assumed_aws_connector_id" {
+  value = harness_platform_connector_aws.sales_eks_aws_assumed.id
+}
